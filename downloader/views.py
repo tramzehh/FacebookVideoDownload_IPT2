90c215af99228d6d5f0f14a19c4b4e57c19b0079
Smart Gill Delarama <smartgill.delarama@gmail.com>
2022-01-04 18:00:25 +0800

    [DOWN-002] - Added stylesheet

--- downloader/views.py
+++ downloader/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+import os
+import re
+import requests
+from datetime import datetime
 
 # Create your views here.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+class IndexPage(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "downloader/home.html")
+
